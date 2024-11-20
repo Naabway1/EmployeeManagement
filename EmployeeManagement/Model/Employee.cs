1d8296f5de3882eda39c6b0f931e99b5c5b861f7
Angelka <Angelka@ANGELKAPC>
2024-11-20 20:08:53 +0500

    Добавьте файлы проекта.

---
+++ EmployeeManagement/Model/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Model
+{
+    internal class Employee
+    {
+        public string Name;
+        public string Surname;
+        public DateTime Birthday;
+        public string Position;
+    }
+}
